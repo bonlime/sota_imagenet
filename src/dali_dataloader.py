29ce702e9857738920a61b753d3b8746821c9ca8
Emil Zakirov <bonlimezak@gmail.com>
2020-04-30 16:49:26 +0000

    add more color augmentations

--- src/dali_dataloader.py
+++ src/dali_dataloader.py
@@ -1,13 +1,14 @@
 """Dali dataloader for imagenet"""
 import math
-from nvidia import dali
+from nvidia.dali import ops
+from nvidia.dali import types
+from nvidia.dali.pipeline import Pipeline
 from nvidia.dali.plugin.pytorch import DALIClassificationIterator
 from pytorch_tools.utils.misc import env_rank, env_world_size
 
 DATA_DIR = "data/"
 
-
-class HybridPipe(dali.pipeline.Pipeline):
+class HybridPipe(Pipeline):
     def __init__(self, train=False, bs=32, workers=4, sz=224, ctwist=True, dali_cpu=False, min_area=0.08):
 
         local_rank, world_size = env_rank(), env_world_size()
@@ -15,7 +16,7 @@ class HybridPipe(dali.pipeline.Pipeline):
         data_dir = DATA_DIR + "320/" if sz < 224 and train else DATA_DIR + "raw-data/"
         data_dir += "train/" if train else "val/"
         # only shuffle train data
-        self.input = dali.ops.FileReader(
+        self.input = ops.FileReader(
             file_root=data_dir,
             random_shuffle=train,
             shard_id=local_rank,
@@ -24,44 +25,51 @@ class HybridPipe(dali.pipeline.Pipeline):
         )
 
         if train:
-            self.decode = dali.ops.ImageDecoderRandomCrop(
+            self.decode = ops.ImageDecoderRandomCrop(
+                output_type=types.RGB,
                 device="cpu" if dali_cpu else "mixed",
-                output_type=dali.types.RGB,
                 random_aspect_ratio=[0.75, 1.25],
                 random_area=[min_area, 1.0],
                 num_attempts=100,
             )
             # resize doesn't preserve aspect ratio on purpose
             # works much better with INTERP_TRIANGULAR
-            self.resize = dali.ops.Resize(
+            self.resize = ops.Resize(
                 device="cpu" if dali_cpu else "gpu",
-                interp_type=dali.types.INTERP_TRIANGULAR,
+                interp_type=types.INTERP_TRIANGULAR,
                 resize_x=sz,
                 resize_y=sz,
             )
         else:
-            self.decode = dali.ops.ImageDecoder(device="mixed", output_type=dali.types.RGB)
+            self.decode = ops.ImageDecoder(device="mixed", output_type=types.RGB)
             # 14% bigger and dividable by 16 then center crop
             crop_size = math.ceil((sz * 1.14 + 8) // 16 * 16)
-            self.resize = dali.ops.Resize(
-                device="gpu", interp_type=dali.types.INTERP_TRIANGULAR, resize_shorter=crop_size,
+            self.resize = ops.Resize(
+                device="gpu", interp_type=types.INTERP_TRIANGULAR, resize_shorter=crop_size,
             )
+        # color augs
+        self.contrast = ops.BrightnessContrast(device="gpu")
+        self.hsv = ops.Hsv(device="gpu")
+        self.rgb2bgr = ops.ColorSpaceConversion(image_type=types.RGB, output_type=types.BGR, device="gpu")
+        self.rgb2gray = ops.ColorSpaceConversion(image_type=types.RGB, output_type=types.GRAY, device="gpu")
+        self.gray2rgb = ops.ColorSpaceConversion(image_type=types.GRAY, output_type=types.RGB, device="gpu")
 
-        self.color_twist = dali.ops.ColorTwist(device="gpu")
-        self.jitter = dali.ops.Jitter(device="gpu")
-        self.normalize = dali.ops.CropMirrorNormalize(
+        self.jitter = ops.Jitter(device="gpu")
+        self.normalize = ops.CropMirrorNormalize(
             device="gpu",
-            output_dtype=dali.types.FLOAT,
             crop=(sz, sz),
-            image_type=dali.types.RGB,
             mean=[0.485 * 255, 0.456 * 255, 0.406 * 255],
             std=[0.229 * 255, 0.224 * 255, 0.225 * 255],
-            output_layout=dali.types.NCHW,
+            image_type=types.RGB,
+            output_dtype=types.FLOAT,
+            output_layout=types.NCHW,
         )
-        self.coin = dali.ops.CoinFlip()
-        self.rng1 = dali.ops.Uniform(range=[0, 1])
-        self.rng2 = dali.ops.Uniform(range=[0.85, 1.15])
-        self.rng3 = dali.ops.Uniform(range=[-15, 15])
+        self.coin = ops.CoinFlip()
+        self.coin03 = ops.CoinFlip(probability=0.3)
+
+        self.rng1 = ops.Uniform(range=[0, 1])
+        self.rng2 = ops.Uniform(range=[0.85, 1.15])
+        self.rng3 = ops.Uniform(range=[-15, 15])
         self.train = train
         self.dali_cpu = dali_cpu
         self.ctwist = ctwist
@@ -77,13 +85,16 @@ class HybridPipe(dali.pipeline.Pipeline):
             images = images.gpu()
         if self.train:
             if self.ctwist:
-                # always improves quiality slightly
-                images = self.color_twist(
-                    images,
-                    saturation=self.rng2(),
-                    contrast=self.rng2(),
-                    brightness=self.rng2(),
-                    hue=self.rng3(),
+                if self.coin03():
+                    images = self.rgb2bgr(images)
+                if self.coin03():
+                    images = self.rgb2gray(images)
+                    images = self.gray2rgb(images)
+                images = self.contrast(
+                    images, contrast=self.rng2(), brightness=self.rng2()
+                )
+                images = self.hsv(
+                    images, hue=self.rng3(), saturation=self.rng2(), value=self.rng2(),
                 )
             # images = self.jitter(images, mask=self.coin())
             images = self.normalize(
