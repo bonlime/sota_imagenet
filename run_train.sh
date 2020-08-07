a1ad07dadc6a47e86680b2e3c891097de52908c2
Emil Zakirov <bonlimezak@gmail.com>
2020-08-07 12:40:10 +0300

    allow different configs in run_train

--- run_train.sh
+++ run_train.sh
@@ -1,7 +1,9 @@
+# $1 is config file
+
 docker run --rm -it --shm-size 8G --gpus '"device=0,1,2,3"' \
   -v $IMAGENET_DIR:/workdir/data/ \
   -v `pwd`/logs/:/workdir/logs \
   -v `pwd`/configs/:/workdir/configs \
   bonlime/imagenet:latest \
  python3 -m torch.distributed.launch --nproc_per_node=4 \
- train.py -c configs/effnetb0.yaml
+ train.py -c $@
