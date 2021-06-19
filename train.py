6000066489acd91e233ef4af42f653cffda90915
Emil Zakirov <bonlimezak@gmail.com>
2021-06-19 18:25:47 +0000

    move to hydra & DALI v1.2

--- train.py
+++ train.py
@@ -1,319 +1,167 @@
 import os
 import sys
-import yaml
-import copy
-import math
-import json
 import time
-import shutil
-import warnings
-from pathlib import Path
+import subprocess
 from loguru import logger
-from datetime import datetime
-
-import timm
 
 import torch
-import torch.nn as nn
-import torch.backends.cudnn as cudnn
-import torch.optim
-import torch.distributed as dist
-
-from apex.parallel import DistributedDataParallel as DDP
-from torch.nn.parallel import DistributedDataParallel as DDP_torch
+from torch.nn.parallel import DistributedDataParallel as DDP
 
 import pytorch_tools as pt
-import pytorch_tools.models as models
 import pytorch_tools.fit_wrapper.callbacks as pt_clb
 from pytorch_tools.fit_wrapper.callbacks import Callback as NoClbk
 
-from pytorch_tools.utils.misc import listify
-from pytorch_tools.optim import optimizer_from_name
+import hydra
+from omegaconf import OmegaConf
 
-from src.arg_parser import parse_args
-from src.dali_dataloader import DaliLoader, ValRectLoader
-from src.utils import HardNegativeWrapper
-from src.utils import FixMatchLoss
-from src.angular_losses import AdaCos
-from src.angular_losses import AdditiveAngularMarginLoss
-from src.angular_losses import SphereLinearLayer
-from src.angular_losses import SphereMLPLayer
-from src.angular_losses import SphereMAELoss
-from src.angular_losses import ArcCosSoftmax
-from src.angular_losses import ArcCosSoftmaxCenter
-from src.angular_losses import MyLoss1
-from src.angular_losses import AdaCosMargin
-
-FLAGS = parse_args()
-# makes it slightly faster
-cudnn.benchmark = True
-if FLAGS.deterministic:
-    pt.utils.misc.set_random_seed(42)
-
-# save script and configs so we can reproduce from logs
-OUTDIR = os.path.join(FLAGS.logdir, FLAGS.name)
-os.makedirs(OUTDIR, exist_ok=True)
-shutil.copy2(os.path.realpath(__file__), f"{OUTDIR}")
-
-yaml.dump(vars(FLAGS), open(OUTDIR + "/config.yaml", "w"), default_flow_style=None)
-# setup logger
-if FLAGS.is_master:
-    config = {
-        "handlers": [
-            {"sink": sys.stdout, "format": "{time:[MM-DD HH:mm:ss]} - {message}"},
-            {"sink": f"{OUTDIR}/logs.txt", "format": "{time:[MM-DD HH:mm:ss]} - {message}"},
-        ],
-    }
-    logger.configure(**config)
-else:
-    logger.configure(handlers=[])
+from src.arg_parser import StrictConfig, DataStage
+from src.dali_dataloader import DaliDataManager
 
 
-def main():
-    logger.info(FLAGS)
+@hydra.main(config_path="./configs", config_name="base")
+def main(cfg: StrictConfig):
+
+    start_time = time.time()
+    # setup distributed args
+    cfg.distributed = cfg.world_size > 1
+    # Only want master rank logging to tensorboard
+    cfg.is_master = not cfg.distributed or cfg.local_rank == 0
+
+    # save hashid and git diff for reproduceability. current dir is already in logs because of Hydra
+    kwargs = {"universal_newlines": True, "stdout": subprocess.PIPE}
+    with open("commit_hash.txt", "w") as f:
+        f.write(subprocess.run(["git", "rev-parse", "--short", "HEAD"], **kwargs).stdout)
+    with open("diff.txt", "w") as f:
+        f.write(subprocess.run(["git", "diff"], **kwargs).stdout)
+
+    # setup loguru logger
+    if cfg.is_master:
+        config = {
+            "handlers": [
+                {"sink": sys.stdout, "format": "{time:[MM-DD HH:mm:ss]} - {message}"},
+                {"sink": f"logs.txt", "format": "{time:[MM-DD HH:mm:ss]} - {message}"},
+            ],
+        }
+        logger.configure(**config)
+    else:
+        logger.configure(handlers=[])
+
+    logger.info(OmegaConf.to_yaml(cfg, resolve=True))
     logger.info(f"Pytorch-tools version: {pt.__version__}")
     logger.info(f"Torch version: {torch.__version__}")
 
-    if FLAGS.distributed:
+    torch.backends.cudnn.benchmark = True
+    if cfg.random_seed is not None:
+        pt.utils.misc.set_random_seed(cfg.random_seed)
+
+    if cfg.distributed:
         logger.info("Distributed initializing process group")
-        torch.cuda.set_device(FLAGS.local_rank)
-        dist.init_process_group(backend="nccl", init_method="env://", world_size=FLAGS.world_size)
+        torch.cuda.set_device(cfg.local_rank)
+        torch.distributed.init_process_group(backend="nccl", init_method="env://", world_size=cfg.world_size)
 
     logger.info("Loading model")
-    if FLAGS.arch.startswith("timm_"):
-        # allow using timms models through config
-        model = timm.models.__dict__[FLAGS.arch[5:]](**FLAGS.model_params)
-    elif FLAGS.arch == "GENet_normal":
-        sys.path.append("/mnt/GPU-Efficient-Networks/")
-        import GENet
+    model = hydra.utils.call(cfg.model)
 
-        model = GENet.genet_normal(pretrained=False)
-    else:
-        model = models.__dict__[FLAGS.arch](**FLAGS.model_params)
-    if FLAGS.weight_standardization:
+    if cfg.weight_standardization:
         model = pt.modules.weight_standartization.conv_to_ws_conv(model)
     model = model.cuda()
 
-    if FLAGS.sigmoid_trick:
-        if hasattr(model, "last_linear"):  # speedup convergence
-            nn.init.constant_(model.last_linear.bias, -4.59)
-            print("Using sigmoid trick")
+    criterion = hydra.utils.call(cfg.criterion).cuda()
+    # filter bn from weight decay by default
+    optimized_params = pt.utils.misc.filter_bn_from_wd(model)
 
-    # define loss function (criterion) and optimizer
-    # it's a good idea to use smooth with mixup but don't force it
-    # FLAGS.smooth = FLAGS.smooth or FLAGS.mixup
-    if FLAGS.criterion == "cce":
-        # dirty way to support older code. TODO: rewrite
-        FLAGS.criterion_params["smoothing"] = 0.1 if FLAGS.smooth else 0.0
-        criterion = pt.losses.CrossEntropyLoss(**FLAGS.criterion_params)
-    elif FLAGS.criterion == "sigmoid":
-        criterion = torch.nn.MultiLabelSoftMarginLoss(**FLAGS.criterion_params)
-    elif FLAGS.criterion == "focal":
-        criterion = pt.losses.FocalLoss(**FLAGS.criterion_params)
-    elif FLAGS.criterion == "kld":  # the most suitable loss for sigmoid output with cutmix
-        criterion = pt.losses.BinaryKLDivLoss(**FLAGS.criterion_params)
-    elif FLAGS.criterion == "adacos":
-        criterion = AdaCos(final_criterion=pt.losses.CrossEntropyLoss(), **FLAGS.criterion_params)
-        model.last_linear = SphereLinearLayer(embedding_size=model.last_linear.weight.size(1), num_classes=1000).cuda()
-    elif FLAGS.criterion == "adacos-margin":
-        criterion = AdaCosMargin(**FLAGS.criterion_params)
-        model.last_linear = SphereLinearLayer(embedding_size=model.last_linear.weight.size(1), num_classes=1000).cuda()
-    elif FLAGS.criterion == "arcface":
-        criterion = AdditiveAngularMarginLoss(final_criterion=pt.losses.CrossEntropyLoss())
-        model.last_linear = SphereLinearLayer(embedding_size=model.last_linear.weight.size(1), num_classes=1000).cuda()
-    elif FLAGS.criterion == "a-softmax":
-        criterion = pt.losses.CrossEntropyLoss(**FLAGS.criterion_params)
-        model.last_linear = SphereLinearLayer(embedding_size=model.last_linear.weight.size(1), num_classes=1000).cuda()
-    elif FLAGS.criterion == "a-focal":  # angular focal
-        criterion = pt.losses.FocalLoss(**FLAGS.criterion_params)
-        model.last_linear = SphereLinearLayer(embedding_size=model.last_linear.weight.size(1), num_classes=1000).cuda()
-    elif FLAGS.criterion == "sphere-mae":
-        criterion = SphereMAELoss()
-        model.last_linear = SphereLinearLayer(embedding_size=model.last_linear.weight.size(1), num_classes=1000).cuda()
-    elif FLAGS.criterion == "arc-softmax":
-        criterion = ArcCosSoftmax(**FLAGS.criterion_params)
-        model.last_linear = SphereLinearLayer(embedding_size=model.last_linear.weight.size(1), num_classes=1000).cuda()
-    elif FLAGS.criterion == "arc-softmax-center":
-        criterion = ArcCosSoftmaxCenter(**FLAGS.criterion_params)
-        model.last_linear = SphereLinearLayer(embedding_size=model.last_linear.weight.size(1), num_classes=1000).cuda()
-    elif FLAGS.criterion == "my_loss_1":
-        criterion = MyLoss1(**FLAGS.criterion_params)
-        model.last_linear = SphereLinearLayer(embedding_size=model.last_linear.weight.size(1), num_classes=1000).cuda()
-    elif FLAGS.criterion == "mlp_adacos":
-        criterion = AdaCos(final_criterion=pt.losses.CrossEntropyLoss(), **FLAGS.criterion_params)
-        model.last_linear = SphereMLPLayer(
-            embedding_size=model.last_linear.weight.size(1),
-            num_classes=1000,
-            hidden_size=4096,
-            act="hswish",
-            val_projector=True,
-        ).cuda()
-    elif FLAGS.fixmatch:
-        logger.info(f"Using special fixmatch criterion")
-        criterion = FixMatchLoss(**FLAGS.criterion_params)
-    criterion = criterion.cuda()
-    optim_params = pt.utils.misc.filter_bn_from_wd(model) if FLAGS.no_bn_wd else model.parameters()
     # if criterion has it's own params, also optimize them
-    optim_params[1]["params"].extend(list(criterion.parameters()))
+    optimized_params[1]["params"].extend(list(criterion.parameters()))
 
-    if FLAGS.hard_pct > 0:  # maybe wrap with HNM
-        criterion = HardNegativeWrapper(criterion, FLAGS.hard_pct)
     # start with 0 lr. Scheduler will change this later
-    optimizer = optimizer_from_name(FLAGS.optim)(
-        optim_params, lr=0, weight_decay=FLAGS.weight_decay, **FLAGS.optim_params
-    )
+    optimizer = hydra.utils.call(cfg.optim, optimized_params)
+
     # need to log number of parameters after creating criterion because it may change in the process
     # for example because of MLP layer
     logger.info(f"Model params: {pt.utils.misc.count_parameters(model)[0]/1e6:.2f}M")
-    if FLAGS.resume:
-        checkpoint = torch.load(FLAGS.resume, map_location=lambda storage, loc: storage.cuda(FLAGS.local_rank),)
+
+    if cfg.run.resume:
+        checkpoint = torch.load(cfg.run.resume, map_location=lambda storage, loc: storage.cuda(cfg.local_rank))
         model.load_state_dict(checkpoint["state_dict"], strict=False)
-        FLAGS.start_epoch = checkpoint["epoch"]
-        print("Checkpoint best epoch: ", checkpoint["epoch"])
+        if cfg.run.load_start_epoch:
+            cfg.run.start_epoch = checkpoint["epoch"]
         try:
             optimizer.load_state_dict(checkpoint["optimizer"])
         except:  # may raise an error if another optimzer was used
-            logger.info("Failed to load state dict into optimizer")
-
-    if FLAGS.lookahead:
-        optimizer = pt.optim.Lookahead(optimizer, la_alpha=0.5)
+            logger.info("Failed to load state dict into optimizer. It wasn't saved or optimizer has changed")
 
     # Important to create EMA Callback after cuda() and AMP but before DDP wrapper
-    ema_clb = pt_clb.ModelEma(model, FLAGS.ema_decay) if FLAGS.ema_decay else NoClbk()
-    if FLAGS.distributed:
-        model = DDP_torch(model, device_ids=[FLAGS.local_rank])
+    ema_clb = pt_clb.ModelEma(model, cfg.run.ema_decay) if cfg.run.ema_decay else NoClbk()
+    if cfg.distributed:
+        model = DDP(model, device_ids=[cfg.local_rank])
 
-    # data phases are parsed from start and shedule phases are parsed from the end
-    # it allows mixtures like this: [{ep:0, bs:16, sz:128}, {ep:0, lr:1, mom:0.9}]
-    dm = DaliDataManager(FLAGS.phases)  # + FLAGS.start_epoch here
+    # current dir is already inside logs because of hydra
+    model_saver = pt_clb.CheckpointSaver(os.getcwd(), save_name="model.chpn") if cfg.is_master else NoClbk()
 
-    model_saver = pt_clb.CheckpointSaver(OUTDIR, save_name="model.chpn") if FLAGS.is_master else NoClbk()
+    # nesting dataclasses in List is not currently supported. so do it manually
+    cfg.run.stages = [DataStage(**stg) for stg in cfg.run.stages]
+    logger.info(cfg.run.stages)
+
+    # need to convert stages to dict, so that PhasesScheduler can consume it
+    lr_stages = []
+    for stage in cfg.run.stages:
+        if stage.lr is None:
+            continue
+        lr_stages.append(dict(ep=(stage.start, stage.end), lr=stage.lr, mode=stage.lr_mode))
+    logger.info(f"Learning rate stages: {lr_stages}")
+
     # common callbacks
     callbacks = [
         pt_clb.BatchMetrics([pt.metrics.Accuracy(), pt.metrics.Accuracy(5)]),
-        pt_clb.PhasesScheduler([copy.deepcopy(p) for p in FLAGS.phases if "lr" in p]),
+        pt_clb.PhasesScheduler(lr_stages),
         pt_clb.FileLogger(),
-        pt_clb.Mixup(FLAGS.mixup, 1000) if FLAGS.mixup else NoClbk(),
-        pt_clb.Cutmix(FLAGS.cutmix, 1000) if FLAGS.cutmix else NoClbk(),
         model_saver,  # need to have CheckpointSaver before EMA so moving it here
         ema_clb,  # ModelEMA MUST go after checkpoint saver to work, otherwise it would save main model instead of EMA
     ]
-    if FLAGS.is_master:  # callback for master process
-        callbacks.extend(
-            [pt_clb.Timer(), pt_clb.ConsoleLogger(), pt_clb.TensorBoard(OUTDIR, log_every=25),]
-        )
+    # here we can add any custom callback. MixUp / CutMix is also defined here
+    callbacks += [hydra.utils.call(clb_cfg) for clb_cfg in cfg.run.extra_callbacks]
+    if cfg.is_master:  # callback for master process
+        callbacks.extend([pt_clb.Timer(), pt_clb.ConsoleLogger(), pt_clb.TensorBoard(os.getcwd(), log_every=25)])
     runner = pt.fit_wrapper.Runner(
         model,
         optimizer,
         criterion,
         callbacks=callbacks,
-        use_fp16=FLAGS.opt_level != "O0",
-        accumulate_steps=FLAGS.accumulate_steps,
+        use_fp16=cfg.run.fp16,
+        accumulate_steps=cfg.run.accumulate_steps,
     )
-    if FLAGS.evaluate:
-        dm.set_stage(0)
+
+    # data phases are parsed from start and schedule phases are parsed from the end
+    # it allows mixtures like this: [{ep:0, bs:16, sz:128}, {ep:0, lr:1, mom:0.9}]
+    data_manager = DaliDataManager(cfg)
+
+    if cfg.run.evaluate:
+        data_manager.set_stage(0)
         runner.callbacks.on_begin()
-        runner.evaluate(dm.val_dl)
+        runner.evaluate(data_manager.val_loader)
         return runner.state.loss_meter.avg, runner.state.metric_meters
 
-    for idx in range(len(dm.stages)):
-        dm.set_stage(idx)
+    for idx in range(len(data_manager)):
+        data_manager.set_stage(idx)
         runner.fit(
-            dm.trn_dl,
-            steps_per_epoch=(None, 10)[FLAGS.short_epoch],
-            val_loader=dm.val_dl,
-            val_steps=(None, 20)[FLAGS.short_epoch],
-            epochs=dm.stage_len + dm.stages[idx]["ep"],
-            start_epoch=dm.stages[idx]["ep"],
+            data_manager.loader,
+            steps_per_epoch=(None, 10)[cfg.debug],
+            val_loader=data_manager.val_loader,
+            val_steps=(None, 20)[cfg.debug],
+            epochs=data_manager.end_epoch,
+            start_epoch=data_manager.start_epoch,
         )
     # print number of params again for easier copy-paste
     logger.info(f"Model params: {pt.utils.misc.count_parameters(model)[0]/1e6:.2f}M")
-    return runner.state.val_loss.avg, runner.state.val_metrics
 
-
-class DaliDataManager:
-    """Almost the same as DataManager but lazy and only gets dataloaders when asked"""
-
-    def __init__(self, phases):
-        self.stages = [copy.deepcopy(p) for p in phases if "bs" in p]
-        eps = [listify(p["ep"]) for p in phases]
-        self.tot_epochs = max([max(ep) for ep in eps])
-
-    def set_stage(self, idx):
-        stage = self.stages[idx]
-        self._set_data(stage)
-        if (idx + 1) < len(self.stages):
-            self.stage_len = self.stages[idx + 1]["ep"] - stage["ep"]
-        else:
-            self.stage_len = self.tot_epochs - stage["ep"]
-
-    def _set_data(self, phase):
-        logger.info(f"Dataset changed.\nImage size: {phase['sz']}\nBatch size: {phase['bs']}")
-        # tb.log_size(phase['bs'], phase['sz'])
-        if getattr(self, "trn_dl", None):
-            # remove if exist. prevents DALI errors
-            del self.trn_dl
-            del self.val_dl
-            torch.cuda.empty_cache()
-        self.trn_dl, self.val_dl = self._load_data(**phase)
-
-    def _load_data(self, ep, sz, bs, **kwargs):
-
-        if "lr" in kwargs:
-            del kwargs["lr"]  # in case we mix schedule and data phases
-        if "mom" in kwargs:
-            del kwargs["mom"]  # in case we mix schedule and data phases
-        # change global parameters from phases
-        for k, v in kwargs.items():
-            if hasattr(FLAGS, k):
-                setattr(FLAGS, k, v)
-
-        # 50.000 should be dividable by val_bs * num_gpu
-        # otherwise reduced accuracy differs from acc on 1 gpu
-        val_sz = kwargs.pop("val_sz", sz)  # maybe use differend size for validation
-        if val_sz == 128:
-            val_bs = 500
-        elif val_sz == 224:
-            val_bs = 250
-        else:
-            val_bs = 125
-        trn_loader = DaliLoader(
-            train=True,
-            bs=bs,
-            workers=FLAGS.workers,
-            sz=sz,
-            ctwist=FLAGS.ctwist,
-            min_area=FLAGS.min_area,
-            resize_method=FLAGS.resize_method,
-            classes_divisor=FLAGS.classes_divisor,
-            use_tfrecords=FLAGS.use_tfrecords,
-            crop_method=FLAGS.crop_method,
-            jitter=FLAGS.jitter,
-            blur=FLAGS.blur,
-            random_interpolation=FLAGS.random_interpolation,
-            fixmatch=FLAGS.fixmatch,
-        )
-        if FLAGS.rect_validation:
-            val_loader = ValRectLoader(bs=val_bs, workers=FLAGS.workers, sz=val_sz, resize_method=FLAGS.resize_method)
-        else:
-            val_loader = DaliLoader(
-                False,
-                bs=val_bs,
-                workers=FLAGS.workers,
-                sz=val_sz,
-                resize_method=FLAGS.resize_method,
-                classes_divisor=FLAGS.classes_divisor,
-                use_tfrecords=FLAGS.use_tfrecords,
-                crop_method=FLAGS.crop_method,
-            )
-        return trn_loader, val_loader
-
-
-if __name__ == "__main__":
-    start_time = time.time()  # Loading start to after everything is loaded
-    _, metrics = main()
-    # metrics here are already reduced by runner. no need to anything additionally
-    if FLAGS.is_master:
+    if cfg.is_master:
+        # metrics here are already reduced by runner. no need to anything additionally
+        metrics = runner.state.val_metrics
         logger.info(f"Acc@1 {metrics['Acc@1'].avg:.3f} Acc@5 {metrics['Acc@5'].avg:.3f}")
         m = (time.time() - start_time) / 60
         logger.info(f"Total time: {int(m / 60)}h {m % 60:.1f}m")
+
+
+if __name__ == "__main__":
+    # TODO: find out how to return anything from hydra app. right now it always returns None
+    main()
